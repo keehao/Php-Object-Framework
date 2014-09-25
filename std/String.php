30ef94cc9a4cc28a52522438c3b15575151b9aa3
saligia <zhanghaotian2012@me.com>
2014-09-25 18:45:22 +0800

    字符串类

---
+++ std/String.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saligia
+ * Date: 14-9-25
+ * Time: 下午6:33
+ */
+
+namespace std;
+
+
+class String
+{
+    public function __construct($string = '')
+    {
+
+    }
+
+    public function value()
+    {
+
+    }
+
+    public function toInt()
+    {
+
+    }
+
+    public function toString()
+    {
+
+    }
+} 
